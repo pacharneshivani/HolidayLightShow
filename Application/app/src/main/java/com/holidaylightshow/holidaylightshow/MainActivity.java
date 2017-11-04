9b2752b1310b1b709e71395ce4641d8b497baf16
pacharneshivani <pacharneshivani@yahoo.com>
2017-11-03 21:35:20 -0500

    First commit

---
+++ Application/app/src/main/java/com/holidaylightshow/holidaylightshow/MainActivity.java
@@ -0,0 +1,36 @@
+package com.holidaylightshow.holidaylightshow;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Holiday Light Show");
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        TextView description = (TextView) findViewById(R.id.text_description);
+        description.setText("Immerse your holidays with a brilliant light show! Easily customizable light show that will dazzle your senses.\n");
+        Button connect = (Button) findViewById(R.id.button_connect);
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
---
+++ Application/app/src/main/java/com/holidaylightshow/holidaylightshow/SettingsActivity.java
@@ -0,0 +1,72 @@
+package com.holidaylightshow.holidaylightshow;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import java.nio.ByteBuffer;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Settings");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+    }
+
+    public void playLightShow(View v) {
+        float f = sharedPreferences.getFloat("frequency_", 0.0f);
+        int frequencyScaled = (int) ((f / 1.0f) * 100.0f);
+        byte frequency = (byte) frequencyScaled;
+        int musicInt = Integer.valueOf(sharedPreferences.getString("music", ""));
+        byte music = (byte) musicInt;
+        int shapeInt = Integer.valueOf(sharedPreferences.getString("shapes", ""));
+        byte shape = (byte) shapeInt;
+        int colorInt = Integer.valueOf(sharedPreferences.getInt("colors", 0));
+        Log.d("color", String.valueOf(colorInt));
+        byte[] color = ByteBuffer.allocate(4).putInt(colorInt).array();
+        int patternInt = Integer.valueOf(sharedPreferences.getString("pattern", ""));
+        byte pattern = (byte) patternInt;
+        UDPServer udpServer = new UDPServer(frequency, music, shape, color, pattern);
+        udpServer.connect();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            View rootView = getView();
+            ListView list = (ListView) rootView.findViewById(android.R.id.list);
+            list.setDivider(null);
+
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
