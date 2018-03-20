cb169251690309fc2341bc82f4e853a1fb4c3b69
Shivani Pacharne <pacharneshivani@yahoo.com>
2018-03-20 02:09:31 -0400

    Update SettingsActivity.java

--- Application/app/src/main/java/com/holidaylightshow/holidaylightshow/SettingsActivity.java
+++ Application/app/src/main/java/com/holidaylightshow/holidaylightshow/SettingsActivity.java
@@ -2,24 +2,24 @@ package com.holidaylightshow.holidaylightshow;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SeekBar;
-import android.widget.TextView;
-
-import java.nio.ByteBuffer;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private static final String STOP_SIGNAL = "e";
     private SharedPreferences sharedPreferences;
+    private UdpClientHandler udpClientHandler;
+    private UdpClientThread udpClientThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +29,89 @@ public class SettingsActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+        udpClientHandler = new UdpClientHandler(this);
+    }
+
+    private String getSharedPrefStatusString(boolean status, String key) {
+        String result = "0";
+
+        if (status) {
+            if (key.contains("color")) {
+                result = Integer.toHexString(sharedPreferences.getInt(key, 0));
+            } else {
+                result = sharedPreferences.getString(key, "");
+            }
+        }
+
+        return result;
     }
 
     public void playLightShow(View v) {
-        byte frequency = 0;
-        /*float f = sharedPreferences.getFloat("frequency_", 0.0f);
-        int frequencyScaled = (int) ((f / 1.0f) * 100.0f);
-        byte frequency = (byte) frequencyScaled;*/
-        int musicInt = Integer.valueOf(sharedPreferences.getString("music", ""));
-        byte music = (byte) musicInt;
-        int shapeInt = Integer.valueOf(sharedPreferences.getString("shapes", ""));
-        byte shape = (byte) shapeInt;
-        int colorInt = Integer.valueOf(sharedPreferences.getInt("colors", 0));
-        Log.d("color", String.valueOf(colorInt));
-        byte[] color = ByteBuffer.allocate(4).putInt(colorInt).array();
-        int patternInt = Integer.valueOf(sharedPreferences.getString("pattern", ""));
-        byte pattern = (byte) patternInt;
+        String frequency = String.valueOf((double) sharedPreferences.getInt("frequency", 0)/10.0);
+        String music = sharedPreferences.getString("music", "");
+
+        boolean status1 = sharedPreferences.getBoolean("status1", false);
+        boolean status2 = sharedPreferences.getBoolean("status2", false);
+        boolean status3 = sharedPreferences.getBoolean("status3", false);
+        boolean status4 = sharedPreferences.getBoolean("status4", false);
+
+        String shape1 = getSharedPrefStatusString(status1, "shape1");
+        String color1 = getSharedPrefStatusString(status1, "color1");
+        String pattern1 = getSharedPrefStatusString(status1, "pattern1");
+
+        String shape2 = getSharedPrefStatusString(status2, "shape2");
+        String color2 = getSharedPrefStatusString(status2, "color2");
+        String pattern2 = getSharedPrefStatusString(status2, "pattern2");
+
+        String shape3 = getSharedPrefStatusString(status3, "shape3");
+        String color3 = getSharedPrefStatusString(status3, "color3");
+        String pattern3 = getSharedPrefStatusString(status3, "pattern3");
+
+        String shape4 = getSharedPrefStatusString(status4, "shape4");
+        String color4 = getSharedPrefStatusString(status4, "color4");
+        String pattern4 = getSharedPrefStatusString(status4, "pattern4");
+
+        String data = frequency + music
+                + shape1 + color1 + pattern1
+                + shape2 + color2 + pattern2
+                + shape3 + color3 + pattern3
+                + shape4 + color4 + pattern4;
+
+        udpClientThread = new UdpClientThread(udpClientHandler, data);
+        udpClientThread.start();
+
+        Button play = (Button) findViewById(R.id.button_play);
+        play.setEnabled(false);
+        updateState("Configuring Light Show");
+    }
+
+    public void stopLightShow(View v) {
+        udpClientThread = new UdpClientThread(udpClientHandler, STOP_SIGNAL);
+        udpClientThread.start();
+
+        updateState("Stopping Light Show");
+    }
+
+    public void updateState(String state){
+        Toast.makeText(getApplicationContext(), state, Toast.LENGTH_SHORT).show();
+    }
+
+    public void clientEnd(){
+        udpClientThread = null;
+
+        Button play = (Button) findViewById(R.id.button_play);
+        play.setEnabled(true);
+        updateState("Send successful");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static class SettingsFragment extends PreferenceFragment {
@@ -60,17 +127,41 @@ public class SettingsActivity extends AppCompatActivity {
             View rootView = getView();
             ListView list = (ListView) rootView.findViewById(android.R.id.list);
             list.setDivider(null);
-
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            this.finish();
-            return true;
+    public static class UdpClientHandler extends Handler {
+
+        private SettingsActivity parent;
+        private static final int UPDATE_STATE = 0;
+        private static final int UPDATE_END = 1;
+
+        public UdpClientHandler(SettingsActivity parent) {
+            super();
+            this.parent = parent;
         }
-        return super.onOptionsItemSelected(item);
+
+        public void sendState(String state){
+            sendMessage(Message.obtain(this, UPDATE_STATE, state));
+        }
+
+        public void sendEndSignal() {
+            sendEmptyMessage(UPDATE_END);
+        }
+
+        @Override public void handleMessage(Message message) {
+            switch (message.what){
+                case UPDATE_STATE:
+                    parent.updateState((String) message.obj);
+                    break;
+                case UPDATE_END:
+                    parent.clientEnd();
+                    break;
+                default:
+                    super.handleMessage(message);
+            }
+        }
+
     }
 
 }
